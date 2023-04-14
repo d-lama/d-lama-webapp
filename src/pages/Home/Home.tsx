9394712e04756048491a231a3fbca3f9232f3c93
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-04-14 22:18:40 +0200

    added home title with add project button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,33 @@
 import './Home.css';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonIcon, IonPage, IonRow } from '@ionic/react';
 import Header from '../../components/Header';
 import { ProjectGrid } from './ProjectGrid';
+import { add } from 'ionicons/icons';
 
 export const Home: React.FC = () => {
+  const handleAddProject = () => {
+    console.log('click');
+  }
+
   return (
     <IonPage>
       <Header />
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol size='10'>
+              <h1 className='home-title'>Projects</h1>
+            </IonCol>
+            <IonCol size='2' className='ion-text-right'>
+              <IonIcon
+                icon={add}
+                onClick={handleAddProject}
+                aria-label='Add a new project'
+                className='home-add-project-icon'
+              />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
         <ProjectGrid />
       </IonContent>
     </IonPage>
