1c24957c1bbd8c70a31635b07c25561ea79838b4
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-04-14 12:46:18 +0200

    changed css classes

--- src/pages/Home/CustomButton.tsx
+++ src/pages/Home/CustomButton.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { IonButton, IonIcon } from '@ionic/react';
-import { play } from 'ionicons/icons';
+import React from 'react'
+import { IonButton, IonIcon } from '@ionic/react'
+import { play } from 'ionicons/icons'
 import './CustomButton.css'
 
 interface CustomButtonProps {
@@ -14,10 +14,10 @@ export const CustomButton: React.FC<CustomButtonProps> = ({ title, progress }) =
     };
 
     return (
-        <IonButton className='btn'>
-            <div className='progressBar' style={progressBarStyle}></div>
-            <span className='title'>{title}</span>
-            <IonIcon icon={play} className='icon' />
+        <IonButton className='customBtn-btn'>
+            <div className='customBtn-progressBar' style={progressBarStyle}></div>
+            <span className='customBtn-title'>{title}</span>
+            <IonIcon icon={play} className='customBtn-icon' />
         </IonButton>
     );
 };
