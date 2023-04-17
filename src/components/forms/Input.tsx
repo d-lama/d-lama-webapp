9a38cceaf2a29a6296d902a6f0bc47d4ce2066f6
vonwareb <vonwareb@students.zhaw.ch>
2023-04-17 20:32:11 +0200

    feature/9-registration-page-mobile - integrated handler for inputfields

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -10,19 +10,27 @@ export enum InputType {
 }
 
 export interface InputProps {
+    name?: string,
+    inputType: InputType,
     inputName: string,
-    placeholder: string,
-    helperText: string,
-    errorText: string
-    inputType: InputType
+    placeholder?: string,
+    change?: any
+    helperText?: string,
+    errorText: string,
+
 }
 
 export function Input(props: InputProps) {
     return (
         <IonItem fill="outline" style={{marginTop: '15px'}} shape='round' mode={"md"}>
             <IonLabel className="ion-text-center" position="floating">{props.inputName}</IonLabel>
-            <IonInput className="ion-text-center" type={props.inputType}
-                      placeholder={props.placeholder}></IonInput>
+            <IonInput
+                className="ion-text-center"
+                name={props.name}
+                type={props.inputType}
+                placeholder={props.placeholder}
+                onIonChange={props.change}
+            ></IonInput>
             <IonNote slot="helper">{props.helperText}</IonNote>
             <IonNote slot="error">{props.errorText}</IonNote>
         </IonItem>
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -14,6 +14,18 @@ import {Button, ButtonType} from "../components/forms/Button";
 
 function Registration() {
     const [labelText, setLabelText] = useState('');
+    const [mask, setMask] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    })
+
+    function handleChange(e: { target: { name: any; value: any; }; }) {
+        console.log(e.target.value)
+        setMask(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
     const handleSubmit = function (event: { preventDefault: () => void; }) {
         // Handle user authentication logic here
         event.preventDefault()
@@ -41,20 +53,46 @@ function Registration() {
                         height: '80vh'
                     }}>
                         <form style={{width: '80%', maxWidth: '400px'}} onSubmit={handleSubmit}>
-                            <Input inputName={"Enter First Name"} placeholder={"Max"} helperText={"Enter a valid name"}
-                                   errorText={"Invalid email"} inputType={InputType.text}/>
-                            <Input inputName={"Enter Last Name"} placeholder={"Muster"}
-                                   helperText={"Enter a valid name"} errorText={"Invalid email"}
-                                   inputType={InputType.text}/>
-                            <Input inputName={"Enter Email"} placeholder={"max.muster@gmail.com"}
-                                   helperText={"Enter a valid email"} errorText={"Invalid password"}
-                                   inputType={InputType.email}/>
-                            <Input inputName={"Enter Password"} placeholder={"**************"}
-                                   helperText={"Enter a valid password"} errorText={"Invalid password"}
-                                   inputType={InputType.password}/>
-                            <Input inputName={"Confirm Password"} placeholder={"**************"}
-                                   helperText={"Confirm the password"} errorText={"Invalid password"}
-                                   inputType={InputType.password}/>
+                            <Input
+                                name={"firstName"}
+                                change={handleChange}
+                                inputName={"Enter First Name"}
+                                placeholder={"Max"}
+                                helperText={"Enter a valid name"}
+                                errorText={"Invalid email"}
+                                inputType={InputType.text}/>
+                            <Input
+                                name={"lastName"}
+                                change={handleChange}
+                                inputName={"Enter Last Name"}
+                                placeholder={"Muster"}
+                                helperText={"Enter a valid name"}
+                                errorText={"Invalid email"}
+                                inputType={InputType.text}/>
+                            <Input
+                                name={"email"}
+                                change={handleChange}
+                                inputName={"Enter Email"}
+                                placeholder={"max.muster@gmail.com"}
+                                helperText={"Enter a valid email"}
+                                errorText={"Invalid password"}
+                                inputType={InputType.email}/>
+                            <Input
+                                name={"password"}
+                                change={handleChange}
+                                inputName={"Enter Password"}
+                                placeholder={"**************"}
+                                helperText={"Enter a valid password"}
+                                errorText={"Invalid password"}
+                                inputType={InputType.password}/>
+                            <Input
+                                name={"password"}
+                                change={handleChange}
+                                inputName={"Confirm Password"}
+                                placeholder={"**************"}
+                                helperText={"Confirm the password"}
+                                errorText={"Invalid password"}
+                                inputType={InputType.password}/>
                             <IonItem id="{{error}}" style={{marginBottom: '15px'}}>
                                 {labelText &&
                                     <IonLabel className="ion-text-center" color="danger">{labelText}</IonLabel>}
