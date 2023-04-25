d30b3047efd8c501af84d07bab9b7b359d91976a
vonwareb <vonwareb@students.zhaw.ch>
2023-04-25 18:12:07 +0200

    feature/7-login-page-mobile-screen - added axios post api call

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,16 +11,37 @@ import {
 } from '@ionic/react';
 import {Input, InputType} from "../components/forms/Input";
 import {Button, ButtonType} from "../components/forms/Button";
+import axios from "axios";
 
-export function Login() {
+export default function Login() {
     const [labelText, setLabelText] = useState('');
-    const handleLogin = function (event: { preventDefault: () => void; }) {
-        // Handle user authentication logic here
-        event.preventDefault()
-        setLabelText('Invalid username or password!');
-        setTimeout(() => {
-            setLabelText('');
-        }, 3000);
+    const [mask, setMask] = useState({
+        email: "",
+        password: "",
+    })
+
+    function handleChange(e: { target: { name: any; value: any; }; }) {
+        console.log(e.target.value)
+        setMask(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleLogin = function (e:React.SyntheticEvent) {
+        e.preventDefault()
+        //TODO: insert correct URL
+        axios.post('/api/login', {
+            email: mask.email,
+            password: mask.password,
+        })
+            .then(function () {
+                window.location.href = '/home';
+            })
+            .catch(function (error) {
+                console.log(error);
+                setLabelText('Invalid email or password!');
+                setTimeout(() => {
+                    setLabelText('');
+                }, 3000);
+            });
     }
 
     return (
@@ -41,12 +62,22 @@ export function Login() {
                         height: '80vh'
                     }}>
                         <form style={{width: '80%', maxWidth: '400px'}} onSubmit={handleLogin}>
-                            <Input inputName={"Email"} placeholder={"max.muster@gmail.com"}
-                                   helperText={"Enter a valid email"} errorText={"Invalid email"}
-                                   inputType={InputType.email}/>
-                            <Input inputName={"Password"} placeholder={"**************"}
-                                   helperText={"Enter a valid password"} errorText={"error"}
-                                   inputType={InputType.password}/>
+                            <Input
+                                name={"email"}
+                                inputName={"Email"}
+                                change={handleChange}
+                                placeholder={"max.muster@gmail.com"}
+                                helperText={"Enter a valid email"}
+                                errorText={"Invalid email"}
+                                inputType={InputType.email}/>
+                            <Input
+                                name={"password"}
+                                inputName={"Password"}
+                                change={handleChange}
+                                placeholder={"**************"}
+                                helperText={"Enter a valid password"}
+                                errorText={"error"}
+                                inputType={InputType.password}/>
                             <IonItem id="{{error}}" style={{marginBottom: '20px'}}>
                                 {labelText &&
                                     <IonLabel className="ion-text-center" color="danger">{labelText}</IonLabel>}
@@ -63,4 +94,3 @@ export function Login() {
     );
 }
 
-export default Login;
