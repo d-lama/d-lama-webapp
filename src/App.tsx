fc553c2104aed9864762c8b514cb69a2556eafc9
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-05-03 17:07:24 +0200

    added api url, changed login api connection url
    formatter changes every line 🤪

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,57 @@
-import {Redirect, Route} from 'react-router-dom';
-import {IonApp, IonRouterOutlet, setupIonicReact} from '@ionic/react';
-import {IonReactRouter} from '@ionic/react-router';
-import Home from './pages/Home';
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Redirect, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/display.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import React from "react";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import RegistrationSucceed from "./pages/RegistrationSucceed";
-import React from "react";
+import "./theme/variables.css";
 
 setupIonicReact();
 
+export const API_URL = "http://backend.dlama-stage.pm4.init-lab.ch/api";
+
 const App: React.FC = () => (
-    <IonApp>
-        <IonReactRouter>
-            <IonRouterOutlet>
-                <Route exact path="/home">
-                    <Home/>
-                </Route>
-                <Route exact path="/registration">
-                    <Registration/>
-                </Route>
-                <Route exact path="/login">
-                    <Login/>
-                </Route>
-                <Route exact path="/registrationsucceed">
-                    <RegistrationSucceed/>
-                </Route>
-                <Route exact path="/">
-                    <Redirect to="/home"/>
-                </Route>
-            </IonRouterOutlet>
-        </IonReactRouter>
-    </IonApp>
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/registration">
+          <Registration />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/registrationsucceed">
+          <RegistrationSucceed />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
 );
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,94 +1,122 @@
-import React, {useState} from 'react';
-import './Login.css';
 import {
-    IonContent,
-    IonHeader,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonTitle,
-    IonToolbar
-} from '@ionic/react';
-import {Input, InputType} from "../components/forms/Input";
-import {Button, ButtonType} from "../components/forms/Button";
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import axios from "axios";
+import React, { useState } from "react";
+import { API_URL } from "../App";
+import { Button, ButtonType } from "../components/forms/Button";
+import { Input, InputType } from "../components/forms/Input";
+import "./Login.css";
 
 export default function Login() {
-    const [labelText, setLabelText] = useState('');
-    const [mask, setMask] = useState({
-        email: "",
-        password: "",
-    })
+  const [labelText, setLabelText] = useState("");
+  const [mask, setMask] = useState({
+    email: "",
+    password: "",
+  });
 
-    function handleChange(e: { target: { name: any; value: any; }; }) {
-        setMask(prev => ({...prev, [e.target.name]: e.target.value}))
-    }
+  function handleChange(e: { target: { name: any; value: any } }) {
+    setMask((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
 
-    const handleLogin = function (e:React.SyntheticEvent) {
-        e.preventDefault()
-        //TODO: insert correct URL
-        axios.post('/api/login', {
-            email: mask.email,
-            password: mask.password,
-        })
-            .then(function () {
-                window.location.href = '/home';
-            })
-            .catch(function (error) {
-                setLabelText('Invalid email or password!');
-                setTimeout(() => {
-                    setLabelText('');
-                }, 3000);
-            });
-    }
+  const handleLogin = function (e: React.SyntheticEvent) {
+    e.preventDefault();
+    axios
+      .post(API_URL + "/user/authToken", {
+        email: mask.email,
+        password: mask.password,
+      })
+      .then(() => {
+        window.location.href = "/home";
+      })
+      .catch((error) => {
+        if (axios.isAxiosError(error)) {
+          setLabelText(error.message);
+        } else {
+          setLabelText("Invalid email or password!");
+          setTimeout(() => {
+            setLabelText("");
+          }, 3000);
+        }
+      });
+  };
 
-    return (
-        <>
-            <IonPage>
-                <IonHeader class="ion-no-border" mode={"md"}>
-                    <IonToolbar>
-                        <IonTitle style={{fontSize: '3rem', fontWeight: 'bold', marginTop: '60px'}}
-                                  className="ion-text-center">D-LAMA</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        height: '80vh'
-                    }}>
-                        <form style={{width: '80%', maxWidth: '400px'}} onSubmit={handleLogin}>
-                            <Input
-                                name={"email"}
-                                inputName={"Email"}
-                                change={handleChange}
-                                placeholder={"max.muster@gmail.com"}
-                                helperText={"Enter a valid email"}
-                                errorText={"Invalid email"}
-                                inputType={InputType.email}/>
-                            <Input
-                                name={"password"}
-                                inputName={"Password"}
-                                change={handleChange}
-                                placeholder={"**************"}
-                                helperText={"Enter a valid password"}
-                                errorText={"error"}
-                                inputType={InputType.password}/>
-                            <IonItem id="{{error}}" style={{marginBottom: '20px'}}>
-                                {labelText &&
-                                    <IonLabel className="ion-text-center" color="danger">{labelText}</IonLabel>}
-                            </IonItem>
-                            <Button buttonType={ButtonType.submit} buttonText={"Login"} color={"primary"}></Button>
-                            <Button link={"/registration"} buttonText={"Sign Up"}
-                                    buttonType={ButtonType.button}></Button>
-                        </form>
-                    </div>
-                </IonContent>
-            </IonPage>
-        </>
-
-    );
+  return (
+    <>
+      <IonPage>
+        <IonHeader class="ion-no-border" mode={"md"}>
+          <IonToolbar>
+            <IonTitle
+              style={{
+                fontSize: "3rem",
+                fontWeight: "bold",
+                marginTop: "60px",
+              }}
+              className="ion-text-center"
+            >
+              D-LAMA
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "80vh",
+            }}
+          >
+            <form
+              style={{ width: "80%", maxWidth: "400px" }}
+              onSubmit={handleLogin}
+            >
+              <Input
+                name={"email"}
+                inputName={"Email"}
+                change={handleChange}
+                placeholder={"max.muster@gmail.com"}
+                helperText={"Enter a valid email"}
+                errorText={"Invalid email"}
+                inputType={InputType.email}
+              />
+              <Input
+                name={"password"}
+                inputName={"Password"}
+                change={handleChange}
+                placeholder={"**************"}
+                helperText={"Enter a valid password"}
+                errorText={"error"}
+                inputType={InputType.password}
+              />
+              <IonItem id="{{error}}" style={{ marginBottom: "20px" }}>
+                {labelText && (
+                  <IonLabel className="ion-text-center" color="danger">
+                    {labelText}
+                  </IonLabel>
+                )}
+              </IonItem>
+              <Button
+                buttonType={ButtonType.submit}
+                buttonText={"Login"}
+                color={"primary"}
+              ></Button>
+              <Button
+                link={"/registration"}
+                buttonText={"Sign Up"}
+                buttonType={ButtonType.button}
+              ></Button>
+            </form>
+          </div>
+        </IonContent>
+      </IonPage>
+    </>
+  );
 }
-
