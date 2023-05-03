944f1969fb209bcf060513a295463cf085b2b5a0
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-05-03 17:26:07 +0200

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,10 +28,18 @@ export default function Login() {
   const handleLogin = function (e: React.SyntheticEvent) {
     e.preventDefault();
     axios
-      .post(API_URL + "/user/authToken", {
-        email: mask.email,
-        password: mask.password,
-      })
+      .post(
+        API_URL + "/user/authToken",
+        {
+          email: mask.email,
+          password: mask.password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
+        }
+      )
       .then(() => {
         window.location.href = "/home";
       })
