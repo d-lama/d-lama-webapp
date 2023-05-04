ae5ba0fe44453153a46bc963eef4eabe59a3be25
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-05-04 09:30:02 +0200

    added api url, changed registration api connection url
    added birthdate input field

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,38 +1,45 @@
-import {IonInput, IonItem, IonLabel, IonNote} from "@ionic/react";
+import { IonInput, IonItem, IonLabel, IonNote } from "@ionic/react";
 import React from "react";
 
 export enum InputType {
-    email = "email",
-    number = "number",
-    password = "password",
-    text = "text",
-    url = "url",
+  email = "email",
+  number = "number",
+  password = "password",
+  text = "text",
+  url = "url",
+  date = "date",
 }
 
 export interface InputProps {
-    name?: string,
-    inputType: InputType,
-    inputName: string,
-    placeholder?: string,
-    change?: any
-    helperText?: string,
-    errorText: string,
-
+  name?: string;
+  inputType: InputType;
+  inputName: string;
+  placeholder?: string;
+  change?: any;
+  helperText?: string;
+  errorText: string;
 }
 
 export function Input(props: InputProps) {
-    return (
-        <IonItem fill="outline" style={{marginTop: '15px'}} shape='round' mode={"md"}>
-            <IonLabel className="ion-text-center" position="floating">{props.inputName}</IonLabel>
-            <IonInput
-                className="ion-text-center"
-                name={props.name}
-                type={props.inputType}
-                placeholder={props.placeholder}
-                onIonChange={props.change}
-            ></IonInput>
-            <IonNote slot="helper">{props.helperText}</IonNote>
-            <IonNote slot="error">{props.errorText}</IonNote>
-        </IonItem>
-    )
+  return (
+    <IonItem
+      fill="outline"
+      style={{ marginTop: "15px" }}
+      shape="round"
+      mode={"md"}
+    >
+      <IonLabel className="ion-text-center" position="floating">
+        {props.inputName}
+      </IonLabel>
+      <IonInput
+        className="ion-text-center"
+        name={props.name}
+        type={props.inputType}
+        placeholder={props.placeholder}
+        onIonChange={props.change}
+      ></IonInput>
+      <IonNote slot="helper">{props.helperText}</IonNote>
+      <IonNote slot="error">{props.errorText}</IonNote>
+    </IonItem>
+  );
 }
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,137 +1,177 @@
-import React, {useState} from 'react';
 import {
-    IonContent,
-    IonHeader,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonTitle,
-    IonToolbar
-} from '@ionic/react';
-import {Input, InputType} from "../components/forms/Input";
-import {Button, ButtonType} from "../components/forms/Button";
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import axios from "axios";
-
+import React, { useState } from "react";
+import { API_URL } from "../App";
+import { Button, ButtonType } from "../components/forms/Button";
+import { Input, InputType } from "../components/forms/Input";
 
 function Registration() {
-    const [labelText, setLabelText] = useState('');
-    const [mask, setMask] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: ""
-    })
+  const [labelText, setLabelText] = useState("");
+  const [mask, setMask] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    birthDate: "",
+  });
 
-    function isValidEmail(email: string) {
-        return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
+  function isValidEmail(email: string) {
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  }
+
+  function handleChange(e: { target: { name: any; value: any } }) {
+    setMask((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+  const handleSubmit = function (e: React.SyntheticEvent) {
+    e.preventDefault();
+    if (!isValidEmail(mask.email)) {
+      setLabelText("Invalid email!");
+      return;
     }
-
-    function handleChange(e: { target: { name: any; value: any; }; }) {
-        setMask(prev => ({...prev, [e.target.name]: e.target.value}))
+    if (mask.password !== mask.confirmPassword) {
+      setLabelText("Confirm password is not the same!");
+      return;
     }
-
-    const handleSubmit = function (e: React.SyntheticEvent) {
-        e.preventDefault()
-        if (!isValidEmail(mask.email)) {
-            setLabelText('Invalid email!');
-            return;
+    axios
+      .post(API_URL + "/user", {
+        firstName: mask.firstName,
+        lastName: mask.lastName,
+        email: mask.email,
+        password: mask.password,
+        confirmPassword: mask.confirmPassword,
+        birthDate: mask.birthDate,
+        isAdmin: false,
+      })
+      .then(() => {
+        window.location.href = "/registrationSucceed";
+      })
+      .catch((error) => {
+        if (axios.isAxiosError(error)) {
+          setLabelText(error.message);
+        } else {
+          setLabelText("Connection failed!");
+          setTimeout(() => {
+            setLabelText("");
+          }, 3000);
         }
-        if (mask.password !== mask.confirmPassword) {
-            setLabelText('Confirm password is not the same!');
-            return;
-        }
-        axios.post('/api/register', {
-            fistname: mask.firstName,
-            lastname: mask.lastName,
-            email: mask.email,
-            password: mask.password,
-            confirmPassword: mask.confirmPassword
+      });
+  };
 
-        })
-            .then(function () {
-                window.location.href = '/registrationSucceed';
-            })
-            .catch(function () {
-                setLabelText('Connection failed!');
-                setTimeout(() => {
-                    setLabelText('');
-                }, 3000);
-            });
-    }
-
-    return (
-        <>
-            <IonPage>
-                <IonHeader class="ion-no-border" mode={"md"}>
-                    <IonToolbar>
-                        <IonTitle style={{fontSize: '3rem', fontWeight: 'bold', marginTop: '60px'}}
-                                  className="ion-text-center">D-LAMA</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent class={"ion-padding"}>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        height: '80vh'
-                    }}>
-                        <form style={{width: '80%', maxWidth: '400px'}} data-testid="registration-form"
-                              onSubmit={handleSubmit}>
-                            <Input
-                                name={"firstName"}
-                                change={handleChange}
-                                inputName={"Enter First Name"}
-                                placeholder={"Max"}
-                                helperText={"Enter a valid name"}
-                                errorText={"Invalid email"}
-                                inputType={InputType.text}/>
-                            <Input
-                                name={"lastName"}
-                                change={handleChange}
-                                inputName={"Enter Last Name"}
-                                placeholder={"Muster"}
-                                helperText={"Enter a valid name"}
-                                errorText={"Invalid email"}
-                                inputType={InputType.text}/>
-                            <Input
-                                name={"email"}
-                                change={handleChange}
-                                inputName={"Enter Email"}
-                                placeholder={"max.muster@gmail.com"}
-                                helperText={"Enter a valid email"}
-                                errorText={"Invalid password"}
-                                inputType={InputType.email}/>
-                            <Input
-                                name={"password"}
-                                change={handleChange}
-                                inputName={"Enter Password"}
-                                placeholder={"**************"}
-                                helperText={"Enter a valid password"}
-                                errorText={"Invalid password"}
-                                inputType={InputType.password}/>
-                            <Input
-                                name={"confirmPassword"}
-                                change={handleChange}
-                                inputName={"Confirm Password"}
-                                placeholder={"**************"}
-                                helperText={"Confirm the password"}
-                                errorText={"Invalid password"}
-                                inputType={InputType.password}/>
-                            <IonItem id="{{error}}" style={{marginBottom: '15px'}}>
-                                {labelText &&
-                                    <IonLabel className="ion-text-center" color="danger">{labelText}</IonLabel>}
-                            </IonItem>
-                            <Button data-testid="register-button" buttonText={"Register"} buttonType={ButtonType.submit}
-                                    color={"primary"}></Button>
-                        </form>
-                    </div>
-                </IonContent>
-            </IonPage>
-        </>
-
-    );
+  return (
+    <>
+      <IonPage>
+        <IonHeader class="ion-no-border" mode={"md"}>
+          <IonToolbar>
+            <IonTitle
+              style={{
+                fontSize: "3rem",
+                fontWeight: "bold",
+                marginTop: "60px",
+              }}
+              className="ion-text-center"
+            >
+              D-LAMA
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent class={"ion-padding"}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "80vh",
+            }}
+          >
+            <form
+              style={{ width: "80%", maxWidth: "400px" }}
+              data-testid="registration-form"
+              onSubmit={handleSubmit}
+            >
+              <Input
+                name={"firstName"}
+                change={handleChange}
+                inputName={"Enter First Name"}
+                placeholder={"Max"}
+                helperText={"Enter a valid name"}
+                errorText={"Invalid email"}
+                inputType={InputType.text}
+              />
+              <Input
+                name={"lastName"}
+                change={handleChange}
+                inputName={"Enter Last Name"}
+                placeholder={"Muster"}
+                helperText={"Enter a valid name"}
+                errorText={"Invalid email"}
+                inputType={InputType.text}
+              />
+              <Input
+                name={"email"}
+                change={handleChange}
+                inputName={"Enter Email"}
+                placeholder={"max.muster@gmail.com"}
+                helperText={"Enter a valid email"}
+                errorText={"Invalid password"}
+                inputType={InputType.email}
+              />
+              <Input
+                name={"password"}
+                change={handleChange}
+                inputName={"Enter Password"}
+                placeholder={"**************"}
+                helperText={"Enter a valid password"}
+                errorText={"Invalid password"}
+                inputType={InputType.password}
+              />
+              <Input
+                name={"confirmPassword"}
+                change={handleChange}
+                inputName={"Confirm Password"}
+                placeholder={"**************"}
+                helperText={"Confirm the password"}
+                errorText={"Invalid password"}
+                inputType={InputType.password}
+              />
+              <Input
+                name={"birthDate"}
+                change={handleChange}
+                inputName={"Birthdate"}
+                placeholder={""}
+                helperText={"Enter a valid birth date"}
+                errorText={"Invalid date"}
+                inputType={InputType.date}
+              />
+              <IonItem id="{{error}}" style={{ marginBottom: "15px" }}>
+                {labelText && (
+                  <IonLabel className="ion-text-center" color="danger">
+                    {labelText}
+                  </IonLabel>
+                )}
+              </IonItem>
+              <Button
+                data-testid="register-button"
+                buttonText={"Register"}
+                buttonType={ButtonType.submit}
+                color={"primary"}
+              ></Button>
+            </form>
+          </div>
+        </IonContent>
+      </IonPage>
+    </>
+  );
 }
 
 export default Registration;
