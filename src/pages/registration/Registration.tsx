62551bdb48b7f3cbc03247e4481ad63b999e4770
vonwareb <vonwareb@students.zhaw.ch>
2023-05-08 23:07:59 +0200

    feat/11-registration-page-desktop-screen - added RegistrationDesktop.tsx, RegistrationMobile.tsx - Registration thereby divided into mobile and desktop view. - Logic of registration remains in Registration.tsx -added RegistrationDesktop.css for style of RegistrationDesktop view

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,16 +1,7 @@
 import React, {useState} from 'react';
-import {
-    IonContent,
-    IonHeader,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonTitle,
-    IonToolbar
-} from '@ionic/react';
-import {Input, InputType} from "../../components/forms/Input";
-import {Button, ButtonType} from "../../components/forms/Button";
 import axios from "axios";
+import RegistrationMobile from "./RegistrationMobile";
+import RegistrationDesktop from "./RegistrationDesktop";
 
 
 function Registration() {
@@ -60,75 +51,29 @@ function Registration() {
             });
     }
 
+    const isDesktop = window.innerWidth >= 768;
+
     return (
         <>
-            <IonPage>
-                <IonHeader class="ion-no-border" mode={"md"}>
-                    <IonToolbar>
-                        <IonTitle style={{fontSize: '3rem', fontWeight: 'bold', marginTop: '60px'}}
-                                  className="ion-text-center">D-LAMA</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent class={"ion-padding"}>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        height: '80vh'
-                    }}>
-                        <form style={{width: '80%', maxWidth: '400px'}} data-testid="registration-form"
-                              onSubmit={handleSubmit}>
-                            <Input
-                                name={"firstName"}
-                                change={handleChange}
-                                inputName={"Enter First Name"}
-                                placeholder={"Max"}
-                                helperText={"Enter a valid name"}
-                                errorText={"Invalid email"}
-                                inputType={InputType.text}/>
-                            <Input
-                                name={"lastName"}
-                                change={handleChange}
-                                inputName={"Enter Last Name"}
-                                placeholder={"Muster"}
-                                helperText={"Enter a valid name"}
-                                errorText={"Invalid email"}
-                                inputType={InputType.text}/>
-                            <Input
-                                name={"email"}
-                                change={handleChange}
-                                inputName={"Enter Email"}
-                                placeholder={"max.muster@gmail.com"}
-                                helperText={"Enter a valid email"}
-                                errorText={"Invalid password"}
-                                inputType={InputType.email}/>
-                            <Input
-                                name={"password"}
-                                change={handleChange}
-                                inputName={"Enter Password"}
-                                placeholder={"**************"}
-                                helperText={"Enter a valid password"}
-                                errorText={"Invalid password"}
-                                inputType={InputType.password}/>
-                            <Input
-                                name={"confirmPassword"}
-                                change={handleChange}
-                                inputName={"Confirm Password"}
-                                placeholder={"**************"}
-                                helperText={"Confirm the password"}
-                                errorText={"Invalid password"}
-                                inputType={InputType.password}/>
-                            <IonItem id="{{error}}" style={{marginBottom: '15px'}}>
-                                {labelText &&
-                                    <IonLabel className="ion-text-center" color="danger">{labelText}</IonLabel>}
-                            </IonItem>
-                            <Button data-testid="register-button" buttonText={"Register"} buttonType={ButtonType.submit}
-                                    color={"primary"}></Button>
-                        </form>
-                    </div>
-                </IonContent>
-            </IonPage>
+            (
+            <div>
+                {isDesktop ? (
+                    <RegistrationDesktop
+                        mask={mask}
+                        handleChange={handleChange}
+                        handleLogin={handleSubmit}
+                        labelText={labelText}
+                    />
+                ) : (
+                    <RegistrationMobile
+                        mask={mask}
+                        handleChange={handleChange}
+                        handleLogin={handleSubmit}
+                        labelText={labelText}
+                    />
+                )}
+            </div>
+            );
         </>
 
     );
---
+++ src/pages/registration/RegistrationMobile.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import RegistrationMobile from "./RegistrationMobile";
+import React from "react";
+
+test("renders without crashing", () => {
+    const { baseElement } = render(<RegistrationMobile />);
+    expect(baseElement).toBeDefined();
+});
