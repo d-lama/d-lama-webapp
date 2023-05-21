2484b8fba75371ca0f127e9f56a3ba3137adad51
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-05-21 11:04:53 +0200

    added admin project grid and buttons + layout of admin overview

---
+++ src/hooks/useMyProject.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { API_URL } from "../App";
+import { useAuthStore } from "../store/authStore";
+
+export interface IMyProjectData {
+  id: number;
+  ownerId: number;
+  name: string;
+  description: string;
+  isReady: boolean;
+  dataPoints: [];
+  labels: [];
+  creationDate: Date;
+  updateDate: Date;
+}
+
+export const useMyProjects = () => {
+  const { token } = useAuthStore();
+  const [, setLoading] = useState(false);
+
+  const fetchProjects = async (): Promise<IMyProjectData[]> => {
+    setLoading(true);
+    try {
+      return await axios
+        .get<IMyProjectData[]>(`${API_URL}/project/my`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => res.data);
+    } catch (error) {
+      throw new Error("Failed to fetch data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return useQuery<IMyProjectData[], Error>("useMyProjects", fetchProjects, {
+    enabled: true,
+    retry: 3,
+    refetchOnWindowFocus: false,
+  });
+};
--- src/pages/home/HomeAdmin.tsx
+++ src/pages/home/HomeAdmin.tsx
@@ -1,15 +1,95 @@
-import { IonContent, IonPage } from "@ionic/react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonPage,
+  IonRow,
+} from "@ionic/react";
+import { useHistory } from "react-router";
 import { HeaderDesktop } from "../../components/header/HeaderDesktop";
 import { MenuDesktop } from "../../components/menu/MenuDesktop";
+import { ProjectGridAdmin } from "./ProjectGridAdmin";
 
 export const HomeAdmin: React.FC = () => {
+  const history = useHistory();
+
+  const handleCreateProject = () => {
+    history.push("/projectcreation");
+  };
+
   return (
     <>
       <MenuDesktop />
       <IonPage id="main-content">
         <HeaderDesktop />
         <IonContent>
-          <h1 className="home-title">Projects</h1>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="6">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>My Projects</IonCardTitle>
+                    <IonCardSubtitle>
+                      In this section you can see an overview of your current
+                      labeling projects
+                    </IonCardSubtitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <ProjectGridAdmin />
+                    <IonButton
+                      title="Create a new project"
+                      type="button"
+                      size="large"
+                      className="ion-text-center"
+                      expand="full"
+                      shape="round"
+                      color="primary"
+                      onClick={handleCreateProject}
+                      style={{ marginTop: "2rem" }}
+                    >
+                      Create new project
+                    </IonButton>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="6">
+                <IonGrid>
+                  <IonRow>
+                    <IonCol size="12">
+                      <IonCard>
+                        <IonCardHeader>
+                          <IonCardTitle>Statistics</IonCardTitle>
+                          <IonCardSubtitle>
+                            In this section you can see an overview of the
+                            current stats of your labeling projects
+                          </IonCardSubtitle>
+                        </IonCardHeader>
+                        <IonCardContent>{/* Content */}</IonCardContent>
+                      </IonCard>
+                    </IonCol>
+                    <IonCol size="12">
+                      <IonCard>
+                        <IonCardHeader>
+                          <IonCardTitle>Labelers</IonCardTitle>
+                          <IonCardSubtitle>
+                            In this section you can see the labelers that are
+                            labeling your projects
+                          </IonCardSubtitle>
+                        </IonCardHeader>
+                        <IonCardContent>{/* Content */}</IonCardContent>
+                      </IonCard>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
         </IonContent>
       </IonPage>
     </>
