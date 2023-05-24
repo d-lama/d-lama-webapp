614913f7efedd07e0becd2e1e77d8f1e175033b7
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-05-24 21:52:04 +0200

    added lama images

--- src/components/ranking/ProjectUserRanking.tsx
+++ src/components/ranking/ProjectUserRanking.tsx
@@ -7,11 +7,18 @@ import {
 } from "@ionic/react";
 import { star } from "ionicons/icons";
 import { IRankingData } from "../../hooks/Ranking";
-import Lama1Logo from "./lama1.svg";
+import lama1 from "./lama1.jpg";
+import lama2 from "./lama2.jpg";
+import lama3 from "./lama3.jpg";
+import lama4 from "./lama4.jpg";
+import lama5 from "./lama5.jpg";
+import lama6 from "./lama6.jpg";
 
 interface IRankingProps {
   data: IRankingData[];
 }
+
+const lamas = [lama1, lama2, lama3, lama4, lama5, lama6];
 
 const getBestLabeler = (labelers: IRankingData[]) => {
   let best: IRankingData | null = null;
@@ -26,6 +33,11 @@ const getBestLabeler = (labelers: IRankingData[]) => {
 
   return best;
 };
+
+function getRandomLama() {
+  const randomIndex = Math.floor(Math.random() * lamas.length);
+  return lamas[randomIndex];
+}
 
 export const ProjectUserRanking: React.FC<IRankingProps> = ({ data }) => {
   const bestLabeler = getBestLabeler(data);
@@ -47,7 +59,7 @@ export const ProjectUserRanking: React.FC<IRankingProps> = ({ data }) => {
                   color={"warning"}
                 />
               ) : (
-                <img src={Lama1Logo} alt="Lama 1 Logo" />
+                <img src={getRandomLama()} alt="Lama" />
               )}
             </IonThumbnail>
             <IonLabel className={"label-content"} style={{ display: "flex" }}>
